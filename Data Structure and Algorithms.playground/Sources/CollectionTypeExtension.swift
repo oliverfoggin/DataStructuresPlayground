ea912d577aecefab2f59554e04f9cbc0ac31f070
Oliver Foggin <oliverfoggin@me.com>
2015-06-21 23:00:46 +0200

    Removed redundant code

--- Data Structure and Algorithms.playground/Sources/CollectionTypeExtension.swift
+++ Data Structure and Algorithms.playground/Sources/CollectionTypeExtension.swift
@@ -1,17 +1,5 @@
 import Foundation
-import UIKit
 import XCPlayground
-
-public extension MutableCollectionType where Index == Int, Generator.Element : Equatable {
-    func myIndexOf(element: Generator.Element) -> Index? {
-        for i in self.indices {
-            if self[i] == element {
-                return i
-            }
-        }
-        return nil
-    }
-}
 
 public func visualize(identifier: String, array: [Int])
 {
