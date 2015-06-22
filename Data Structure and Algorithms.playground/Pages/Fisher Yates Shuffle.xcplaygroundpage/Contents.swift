eb758e68009d86d041190128bdc77e4cfe263cc6
Oliver Foggin <oliverfoggin@me.com>
2015-06-22 22:57:20 +0200

    Added heap sort and fisher yates shuffle

---
+++ Data Structure and Algorithms.playground/Pages/Fisher Yates Shuffle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var array = Array(0...100)
+
+let arrayView = ArrayStackView()
+arrayView.values = array
+arrayView.captureView("View")
+
+func shuffle(var array: [Int]) -> [Int] {
+    for i in 0..<array.count - 1 {
+        let j = Int(arc4random_uniform(UInt32(array.count - i))) + i
+        swap(&array[i], &array[j])
+        
+        arrayView.values = array
+        arrayView.captureView("View")
+    }
+    return array
+}
+
+shuffle(array)
---
+++ Data Structure and Algorithms.playground/Pages/Heap Sort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+/*:
+[Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+****
+# Heap Sort
+*/
+
+import Foundation
+
+//: Change the **arrayLength** value to see how the length affects performance
+let arrayLength = 50
+let maxNumber = 100
+var array = randomArray(arrayLength, maxNumber: maxNumber)
+
+//: Setting up the view to display the algorithm
+let arrayView = ArrayStackView()
+arrayView.values = array
+arrayView.captureView("View")
+
+func heapsort(inout array: [Int]){
+    heapify(&array)
+    var end = array.count - 1
+    while end > 0 {
+        swap(&array[end--], &array[0])
+        siftDownn(&array, start: 0, end: end)
+        arrayView.values = array
+        arrayView.captureView("View")
+    }
+}
+
+func heapify(inout array: [Int]){
+    var start: Int = Int(floor((Double(array.count) - 2.0) / 2.0))
+    while start >= 0 {
+        siftDownn(&array, start: start, end: array.count - 1)
+        start--
+    }
+}
+
+func siftDownn(inout array: [Int], start: Int, end: Int) {
+    var root = start
+    
+    while root * 2 + 1 <= end {
+        let leftChild = root * 2 + 1
+        let rightChild = leftChild + 1
+        var swap = root
+        
+        if array[swap] < array[leftChild] {
+            swap = leftChild
+        }
+        if rightChild <= end
+            && array[swap] < array[rightChild] {
+                swap = rightChild
+        }
+        if swap == root {
+            return
+        }
+        else {
+            let temp = array[swap]
+            array[swap] = array[root]
+            array[root] = temp
+            root = swap
+            arrayView.values = array
+            arrayView.captureView("View")
+        }
+    }
+}
+
+heapsort(&array)
+
+/*:
+****
+[Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
+*/
